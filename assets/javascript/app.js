b75f53e7fba9e7b875fa7df706d801fbeb71b250
Elma Lopez <elmalopez@Elmas-MacBook-Pro.local>
2018-12-02 18:29:13 -0800

    trying to get my counter to work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,21 @@
 
-        function countDown(seconds,elem){
-            var element = Document.getElementById(elem);
-            element.innerHTMl = "You have '+seconds+' seconds to  the trivia questions";
-            if(seconds < 1) {
-                clearTimeout (timer);
-                element.innerHTML = 'Time is up';
-             }
+        // function countDown(seconds,elem){
+        //     var timer = document.getElementById(status);
+        //     status.innerHTMl = "You have '+seconds+' seconds to  the trivia questions";
+        //     if(seconds < 1) {
+        //         clearTimeout (timer);
+        //         status.innerHTML = 'Time is up';
+        //      }
              
-            seconds--;
-            var timer = setTimeout('countDown('+seconds+', "'+elem+'")', 1000);
-        }
+        //     seconds--;
+        //     var timer = setTimeout('countDown('+seconds+', "'+status+'")', 1000);
+        // }
+// function countDown(secs, myTimer);{
+    var myTimer = document.getElementById('status');
+myTimer.innerHTML = "You have '+secs+' seconds to complete Trivia";
+if (secs < 1){
+    clearTimeout (myTimer)
+    myTimer.innerHTML = "Time is Up";
+}
+secs--;
+var myTimer =setTimeout('countDown('+secs+', "'+myTimer+'")', 3000);
